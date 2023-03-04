e62684984b8a305701afdaecc7a512f82f998813
lydiahelkinz <lydia.lebdiri@helkinz.com>
2023-03-04 14:56:10 +0000

    restard last version

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import './i18n';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
+
 ReactDOM.render(<App />, document.getElementById('root'));
